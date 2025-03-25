64b19139a00aaef5cffe40a25334299f70960d48
Abhinash01 <abinashgupa111@gmail.com>
2025-03-26 01:53:28 +0530

    Final Comparison Practice Sheet

---
+++ FinalComparison.js
@@ -0,0 +1,9 @@
+console.log('FinalComparison.js');
+console.log("Hello" == "Hello");
+console.log("Hello" == "hello");
+console.log("Hello" === "Hello");
+console.log("Hello" === "hello");
+console.log("Hello" != "Hello");
+console.log("Hello" != "hello");
+console.log("Hello" !== "Hello");
+console.log("Hello" !== "hello");
