ac68dc49c767826996e8c5f554e461e7d0a21b9d
Abhinash01 <abinashgupa111@gmail.com>
2025-03-26 15:47:37 +0530

    Number is Positive, Negative, or Zero

---
+++ ifelse2.js
@@ -0,0 +1,9 @@
+//Check if a Number is Positive, Negative, or Zero
+let number = 0;
+if(number > 0) {
+  console.log("The number is positive");
+} else if(number < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("The number is 0");
+}
