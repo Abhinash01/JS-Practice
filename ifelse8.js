80878a606ecb0995a0d1632aacbe01f6f551e9b0
Abhinash01 <abinashgupa111@gmail.com>
2025-03-27 23:49:47 +0530

    Check if a Year is a Century Year (100, 200, 300... etc.)

---
+++ ifelse8.js
@@ -0,0 +1,7 @@
+// Check if a Year is a Century Year (100, 200, 300... etc.)
+let year = prompt("Enter a year: ");
+if (year % 100 == 0) {
+    console.log(year + " is a century year.");
+} else {
+    console.log(year + " is not a century year.");
+} // Output: 2000 is a century year.
