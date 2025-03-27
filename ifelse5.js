3d98c0ed29ec6e1cacbe1e674c1fe0aeb6590509
Abhinash01 <abinashgupa111@gmail.com>
2025-03-27 23:43:21 +0530

    Character is a Vowel or Consonant

---
+++ ifelse5.js
@@ -0,0 +1,8 @@
+//Character is a Vowel or Consonant
+let char = 'a';
+if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+  console.log("The character is a vowel");
+}
+else {
+  console.log("The character is a consonant");
+}
