feab353e49d8179024cae86d3cb41b5318d505b9
Abhinash01 <abinashgupa111@gmail.com>
2025-03-27 23:41:11 +0530

    Smallest of two numbers

---
+++ ifelse4.js
@@ -0,0 +1,9 @@
+//Smallest of two numbers
+let a = 5, b = 10;
+if(a < b) {
+  console.log("The smallest number is a: " + a);
+}
+else {
+    console.log("The smallest number is b: " + b);
+    }
+    
