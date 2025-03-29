b90764337ee6e76c171ed7b765c4e85abdaf4549
Abhinash01 <abinashgupa111@gmail.com>
2025-03-29 23:09:39 +0530

    Divisible by 3 and 5 or not

---
+++ ifelse9.js
@@ -0,0 +1,7 @@
+//Divisible by 3 and 5 or not
+let num = prompt("Enter a Number");
+if (num % 3 == 0 && num % 5 == 0) {
+    console.log("Number is divisible by Both");
+} else {
+    console.log("Number is not divisibe")
+}
