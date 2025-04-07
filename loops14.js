2b6133c481c31f1cc50d0758b00cfcf7072e70d9
Abhinash01 <abinashgupa111@gmail.com>
2025-04-07 23:31:06 +0530

    Find the product of digits of a number

---
+++ loops14.js
@@ -0,0 +1,10 @@
+let number = 234;
+let product = 1;
+
+while (number > 0) {
+  let digit = number % 10;
+  product *= digit;
+  number = Math.floor(number / 10);
+}
+
+console.log("Product of digits:", product);
