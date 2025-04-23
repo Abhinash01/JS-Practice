008c3b578258383712d40c9d0b08dbf1bee08ecc
Abhinash01 <abinashgupa111@gmail.com>
2025-04-23 23:24:23 +0530

    print numbers from 1 to 50, and if a number is divisible by 7, it prints "Lucky Number"

---
+++ uni4.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 50; i++) {
+    if (i % 7 === 0) {
+      console.log("Lucky Number");
+    } else {
+      console.log(i);
+    }
+  }
+  
+
+// Numbers from 1 to 50
+
+// Print numbers from 1 to 50.
+// If the number is divisible by 7, print "Lucky Number" instead.
