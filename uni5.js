5fabb80c0817b5ff8eb32a181c09603415a796f3
Abhinash01 <abinashgupa111@gmail.com>
2025-04-23 23:26:28 +0530

    code to loop from 1 to 30, and print "Special Odd" if the number is odd and divisible by 3, else just print the number

---
+++ uni5.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 !== 0 && i % 3 === 0) {
+      console.log("Special Odd");
+    } else {
+      console.log(i);
+    }
+  }
+
+  
+// Odd Multiples of 3 (1–30)
+
+// Loop from 1 to 30.
+// If the number is odd and divisible by 3, print "Special Odd".
+// Else, print the number.
