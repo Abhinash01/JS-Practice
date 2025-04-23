2aa6eea9712ecb27192a63fec6ce87052b96c1f7
Abhinash01 <abinashgupa111@gmail.com>
2025-04-23 23:40:37 +0530

    1 to 25 and print the range label based on the number

---
+++ uni6.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 25; i++) {
+    if (i >= 1 && i <= 10) {
+      console.log("Low");
+    } else if (i >= 11 && i <= 20) {
+      console.log("Medium");
+    } else if (i >= 21 && i <= 25) {
+      console.log("High");
+    }
+  }
+
+  
+//   Range Identifier (1–25)
+
+// Loop from 1 to 25.
+// If number is between 1–10 → print "Low"
+// 11–20 → print "Medium"
+// 21–25 → print "High"
