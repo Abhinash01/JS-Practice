9ada76dcafb5e0371c18c75cffd14d752493cac1
Abhinash01 <abinashgupa111@gmail.com>
2025-04-24 23:22:14 +0530

    Emoji Weather Forecaster

---
+++ uni18.js
@@ -0,0 +1,12 @@
+
+let weather = "rainy";
+
+if (weather === "sunny") {
+  console.log("☀️ Wear sunglasses");
+} else if (weather === "rainy") {
+  console.log("🌧️ Carry an umbrella");
+} else if (weather === "cloudy") {
+  console.log("☁️ Might be a calm day");
+} else {
+  console.log("🌈 Weather unknown");
+}
