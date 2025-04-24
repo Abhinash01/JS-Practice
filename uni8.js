d4cc0d6576f37839c6704422175f5fde4ecce5b9
Abhinash01 <abinashgupa111@gmail.com>
2025-04-24 23:11:53 +0530

    Traffic Light Simulator

---
+++ uni8.js
@@ -0,0 +1,13 @@
+let light = "yellow"; // try "red", "green", or "yellow"
+
+if (light === "red") {
+  console.log("Stop 🚫");
+} else if (light === "yellow") {
+  console.log("Get Ready ⚠️");
+} else if (light === "green") {
+  console.log("Go ✅");
+} else {
+  console.log("Invalid light color");
+}
+
+
