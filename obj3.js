10f4201f9272cc84aa821ddeea78d07e8ea8c2d1
Abhinash01 <abinashgupa111@gmail.com>
2025-04-25 23:43:20 +0530

    Student Result Object

---
+++ obj3.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Abhinash",
+    marks: {
+      math: 78,
+      english: 82,
+      science: 90
+    }
+  };
+
+  let total = student.marks.math + student.marks.english + student.marks.science;
+  console.log(`Abhinash scored a total of ${total} marks`);
