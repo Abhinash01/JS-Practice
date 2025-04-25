571a7d3db4fe9314ce02ec3a61b5287e86b5ca23
Abhinash01 <abinashgupa111@gmail.com>
2025-04-25 23:44:53 +0530

    Update values and add new key

---
+++ obj4.js
@@ -0,0 +1,14 @@
+let user = {
+    username: "abhinash_21",
+    active: true,
+    points: 80
+  };
+  
+  
+  user.points = 100;
+  user.active = false;
+  
+  
+  user.level = "pro";
+  
+  console.log(user);
