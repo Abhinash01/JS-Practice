0dfe7209e0a5100f7423e9febc9c7e88b6a6efb1
Abhinash01 <abinashgupa111@gmail.com>
2025-05-08 23:14:41 +0530

    Create and Update Object

---
+++ obj8.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "John",
+    age: 25,
+    city: "New York"
+  };
+  
+  person.country = "USA";  // Add new property
+  person.age = 26;         // Update age
+  
+  console.log(person);
+  
