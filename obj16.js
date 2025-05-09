73869a987a7502116afd445085a8122655b1455d
Abhinash01 <abinashgupa111@gmail.com>
2025-05-09 23:31:55 +0530

    Sum Object Values

---
+++ obj16.js
@@ -0,0 +1,11 @@
+let salaries = { John: 100, Ann: 160, Pete: 130 };
+
+function sumSalaries(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  return sum;
+}
+
+console.log(sumSalaries(salaries)); // Output: 390
