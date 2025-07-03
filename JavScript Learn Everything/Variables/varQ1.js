b4ce3a7b3b1d02da7ca02945572690c663471d79
Abhinash Gupta <97685347+Abhinash01@users.noreply.github.com>
2025-07-03 19:13:33 +0000

    Declare your name and city using const , and your age using let .

---
+++ JavScript Learn Everything/Variables/varQ1.js
@@ -0,0 +1,9 @@
+//Declare your name and city using const , and your age using let .
+
+const myName = 'Abhinash';
+const city = 'Cuttack';
+let age = 24;
+
+console.log(myName);
+console.log(city);
+console.log(age);
